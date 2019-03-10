d69b34ab1b01d459ee0cc3c21ddd28b481f72c0a
mulgey <mustafaulgey@hotmail.com>
2019-03-10 18:53:18 +0300

    sunday imp

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,19 @@
 const express = require('express');
 const router = express.Router();
 
+const calcSrcr = function (method, unit, sex, age, weight, srcr) {
+    if (unit === "2") {
+        srcr = srcr / 88.4;
+    }
+    if (method === "Cockcroft-Gault Denklemi") {
+        return (((140 - age) * weight) / (72 * srcr)).toFixed(0);
+        if (sex === "2") {
+            result = (result * 0.85).toFixed(0);
+        }
+    }
+    return result;
+}
+
 // GET /
 router.get('/', function(req, res, next) {
     return res.render("index");
@@ -17,6 +30,8 @@ router.get('/calculator', function(req, res, next) {
 // POST /calculator
 router.post('/calculator', function(req, res, next) {
     console.log("A good start man!");
+    let value = calcSrcr(req.body.method, req.body.unit1, req.body.sex1, req.body.age1, req.body.weight1, req.body.srcr1);
+    console.log(value);
 });
 
 // GET /data_table
