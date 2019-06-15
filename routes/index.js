811bcb8147112cf60d85e16210dc4c7fa18ae98d
knh307 <knh307@konyadh.gov.tr>
2019-06-15 19:39:08 +0300

    shifty imp

--- routes/index.js
+++ routes/index.js
@@ -4,47 +4,29 @@
 const express = require('express');
 const router = express.Router();
 
-/*
-let result;
-
-const calcSrcr = function (method, unit, sex, age, weight, srcr) {
-    if (unit === "2") {
-        srcr = srcr / 88.4;
-    }
-    if (method === "Cockcroft-Gault Denklemi") {
-        result = (((140 - age) * weight) / (72 * srcr)).toFixed(0);
-        if (sex === "2") {
-            result = (result * 0.85).toFixed(0);
-        }
-    }
-    return result;
-}
-
-*/
-
 // GET /
 router.get('/', function(req, res, next) {
-    return res.render("index");
+    return res.render("tr/index");
 });
 
 // GET /calculator
-router.get('/calculator', function(req, res, next) {
-    return res.render("calculator");
+router.get('/tr/calculator', function(req, res, next) {
+    return res.render("tr/calculator");
 });
 
 // GET /data_table
-router.get('/data_table', function(req, res, next) {
-    return res.render("data_table");
+router.get('/tr/data_table', function(req, res, next) {
+    return res.render("tr/data_table");
 });
 
 // GET /about
-router.get('/about', function(req, res, next) {
-    return res.render("about");
+router.get('/tr/about', function(req, res, next) {
+    return res.render("tr/about");
 });
 
 // GET /about
-router.get('/print', function(req, res, next) {
-    return res.render("print");
+router.get('/tr/print', function(req, res, next) {
+    return res.render("tr/print");
 });
 
 module.exports = router;
