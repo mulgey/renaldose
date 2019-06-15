54556d01bd5fe7435bf349b7fbde5c858eae1a84
knh307 <knh307@konyadh.gov.tr>
2019-06-15 20:12:38 +0300

    shifty imp

--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,58 @@
 const express = require('express');
 const router = express.Router();
 
-// GET /
+// TURKISH
+
+// GET tr/
 router.get('/', function(req, res, next) {
     return res.render("tr/index");
 });
 
-// GET /calculator
+// GET tr/calculator
 router.get('/tr/calculator', function(req, res, next) {
     return res.render("tr/calculator");
 });
 
-// GET /data_table
+// GET tr/data_table
 router.get('/tr/data_table', function(req, res, next) {
     return res.render("tr/data_table");
 });
 
-// GET /about
+// GET tr/about
 router.get('/tr/about', function(req, res, next) {
     return res.render("tr/about");
 });
 
-// GET /about
+// GET tr/print
 router.get('/tr/print', function(req, res, next) {
     return res.render("tr/print");
+});
+
+// ENGLISH
+
+// GET en/
+router.get('/en', function(req, res, next) {
+    return res.render("en/index");
+});
+
+// GET en/calculator
+router.get('/en/calculator', function(req, res, next) {
+    return res.render("en/calculator");
+});
+
+// GET en/data_table
+router.get('/en/data_table', function(req, res, next) {
+    return res.render("en/data_table");
+});
+
+// GET en/about
+router.get('/en/about', function(req, res, next) {
+    return res.render("en/about");
+});
+
+// GET en/print
+router.get('/en/print', function(req, res, next) {
+    return res.render("en/print");
 });
 
 module.exports = router;
